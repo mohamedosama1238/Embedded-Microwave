e6219d4f9b8c16d6e895be448c8767430c99df26
Bahy1 <105518579+Bahy1@users.noreply.github.com>
2022-05-22 18:16:33 +0200

    Add files via upload

---
+++ Buzzer.h
@@ -0,0 +1,7 @@
+#include "tm4c123gh6pm.h"
+#include "DIO.h"	
+#include "LED_TIMER.h"
+#include "macros.h"
+
+void Buzzer_Init(unsigned char port_name,unsigned char pin_number);
+void Buzzer_High_low ( unsigned char port_name, unsigned char pin_number);
