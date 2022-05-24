fe5f5fc71919d9378f160415c8483c3acab1c2b3
Bahy1 <105518579+Bahy1@users.noreply.github.com>
2022-05-24 12:00:45 +0200

    Update BUTTON.h

--- BUTTON.h
+++ BUTTON.h
@@ -1,7 +1,8 @@
 #include "tm4c123gh6pm.h"
-#include "DIO.h"
-#include "macros.h"
 
-void Button_Init(unsigned char port_name,unsigned char pin_number);
-unsigned char Button_read(unsigned char port_name,unsigned char pin_number);
-void Button_InitPullUp(unsigned char port_name,unsigned char pin_number);
+
+void Button_Init();
+unsigned char SW1();
+unsigned char SW2();
+void Button_extrnal_Init();
+unsigned char SW3();
