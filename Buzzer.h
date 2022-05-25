8f8c7b4b9a1551fe9f0f84c033bf0c2ee129ab61
Bahy1 <105518579+Bahy1@users.noreply.github.com>
2022-05-25 09:57:06 +0200

    Update Buzzer.h

--- Buzzer.h
+++ Buzzer.h
@@ -1,7 +1,6 @@
 #include "tm4c123gh6pm.h"
-#include "DIO.h"	
-#include "LED_TIMER.h"
-#include "macros.h"
 
-void Buzzer_Init(unsigned char port_name,unsigned char pin_number);
-void Buzzer_High_low ( unsigned char port_name, unsigned char pin_number);
+
+void Buzzer_Init();
+void buzzer_on();
+void buzzer_off();
