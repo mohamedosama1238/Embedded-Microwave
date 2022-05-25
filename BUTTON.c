80c07a105980af060fc2ad2ad6ee522876ca8c72
Bahy1 <105518579+Bahy1@users.noreply.github.com>
2022-05-25 22:34:01 +0200

    Update BUTTON.c

--- BUTTON.c
+++ BUTTON.c
@@ -1,18 +1,18 @@
 #include "tm4c123gh6pm.h"
 
 void Button_Init(){ 
-  SYSCTL_RCGCGPIO_R |= 0x00000020;     // 1) activate clock for Port F
-  while ((SYSCTL_PRGPIO_R & 0x20) == 0)   // allow time for clock to start
-  GPIO_PORTF_LOCK_R = 0x4C4F434B;   // 2) unlock GPIO Port F
-  GPIO_PORTF_CR_R = 0x1F;           // allow changes to PF4-0
-  // only PF0 needs to be unlocked, other bits can't be locked
-  GPIO_PORTF_AMSEL_R = 0x00;        // 3) disable analog on PF
-  GPIO_PORTF_PCTL_R = 0x00000000;   // 4) PCTL GPIO on PF4-0
-  GPIO_PORTF_DIR_R = 0x0E;          // 5) PF4,PF0 in, PF3-1 out
-  GPIO_PORTF_AFSEL_R = 0x00;        // 6) disable alt funct on PF7-0
-  GPIO_PORTF_PUR_R = 0x11;          // enable pull-up on PF0 and PF4
-  GPIO_PORTF_DEN_R = 0x1F;// 7) enable digital I/O on PF4-0
-	GPIO_PORTF_DATA_R &= ~0x0E;	
+  SYSCTL_RCGCGPIO_R |= 0X20;
+	while(!(SYSCTL_PRGPIO_R & 0X20)){};
+	GPIO_PORTF_LOCK_R = GPIO_LOCK_KEY;
+	GPIO_PORTF_CR_R = 0X1F;
+
+	GPIO_PORTF_DEN_R |= 0X1F;
+	GPIO_PORTF_AFSEL_R &= 0XE0;
+	GPIO_PORTF_PCTL_R &= 0X00000;
+	GPIO_PORTF_AMSEL_R &= 0XE0;
+	GPIO_PORTF_DIR_R |= 0X0E;
+	GPIO_PORTF_PUR_R |= 0x11;
+	GPIO_PORTF_DATA_R &= 0XF1;
 }
 
 unsigned char SW1(void){
@@ -24,18 +24,17 @@ return GPIO_PORTF_DATA_R & 0x10;
 
 void Button_extrnal_Init(){
 
-SYSCTL_RCGCGPIO_R |= 0x08;
-while ((SYSCTL_PRGPIO_R & 0x08) == 0)
-GPIO_PORTD_LOCK_R = 0x4C4F434B;
-GPIO_PORTD_CR_R |= 0xFF;
-GPIO_PORTD_AMSEL_R &= 0x00;
-GPIO_PORTD_PCTL_R &= 0x00000000;
-GPIO_PORTD_AFSEL_R &= 0x00;
-GPIO_PORTD_DIR_R &= 0xFE;
-GPIO_PORTD_PUR_R |= 0x01;
-GPIO_PORTD_DEN_R |= 0xFF;
+SYSCTL_RCGCGPIO_R |= 0x01;
+while ((SYSCTL_PRGPIO_R & 0x01) == 0)
+GPIO_PORTA_LOCK_R = 0x4C4F434B;
+GPIO_PORTA_CR_R |= 0x04;
+GPIO_PORTA_AMSEL_R &= ~0x04;
+GPIO_PORTA_PCTL_R &= 0x00000F00;
+GPIO_PORTA_AFSEL_R &= 0xFB;
+GPIO_PORTA_DIR_R &= 0xFB;
+GPIO_PORTA_PUR_R |= 0x04;
+GPIO_PORTA_DEN_R |= 0x04;
 }
 unsigned char SW3(void){
-return (GPIO_PORTD_DATA_R & 0x01);
+return (GPIO_PORTA_DATA_R & 0x04);
 }
-
