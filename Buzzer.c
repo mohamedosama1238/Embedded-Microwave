2e6d156b5979b80b2dea1802454a487c1f429be4
Bahy1 <105518579+Bahy1@users.noreply.github.com>
2022-05-25 09:56:34 +0200

    Update Buzzer.c

--- Buzzer.c
+++ Buzzer.c
@@ -1,18 +1,27 @@
 #include "tm4c123gh6pm.h"
-#include "DIO.h"	
-#include "LED_TIMER.h"
-#include "macros.h"
-void Buzzer_Init(unsigned char port_name,unsigned char pin_number){
-	port_uinit(port_name);
-	DIO_vSET_PIN_DIR(port_name,pin_number,1);
-}
-void Buzzer_High_low ( unsigned char port_name, unsigned char pin_number)
+
+
+	void Buzzer_Init(void)
 {
-	int i;
-	for(i=0; i<=3; i++)
-	{
-		DIO_vWRITE_PIN (port_name,pin_number, 1); //BUZZER PIN IS LOGIC HIGH (BUZZER ON) 
-		Delay(1000);
-		DIO_vWRITE_PIN ( port_name, pin_number, 0); //BUZZER PIN IS LOGIC LOW (BUZZER OFF)
-	}
+    SYSCTL_RCGCGPIO_R |=0x1;
+    while((SYSCTL_PRGPIO_R &0x1)==0);
+
+
+    GPIO_PORTA_CR_R |=0x08;
+    GPIO_PORTA_PCTL_R &=~0x0000F000;
+    GPIO_PORTA_AMSEL_R&=~0x08;
+    GPIO_PORTA_AFSEL_R &=~0x08;
+    GPIO_PORTA_DEN_R |=0xFF;
+    GPIO_PORTA_DIR_R |=0x08;
+	
+
 }
+void buzzer_on(void)
+{
+    GPIO_PORTA_DATA_R |= 0x08;
+}
+void buzzer_off(void)
+{
+    GPIO_PORTA_DATA_R &= ~0x08;
+}
+
